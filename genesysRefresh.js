93ea51291a0e98bd106832b4df2dc1081c6ce280
Joseph Fugata <34955657+josephfugata@users.noreply.github.com>
2024-08-17 19:18:18 +0000

    fix function name

--- genesysRefresh.js
+++ genesysRefresh.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const scrapeLogic = async (res) => {
+const genesysRefresh = async (res) => {
     const browser = await puppeteer.launch({
         args: [
             "--disable-setuid-sandbox",
