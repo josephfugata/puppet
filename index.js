1c30f8395677b3a0c2aba83bb570a1afbf80c9bf
Joseph Fugata <34955657+josephfugata@users.noreply.github.com>
2024-08-17 18:29:25 +0000

    rearrange code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
-
-const PORT = process.env.PORT || 4000;
+const { scrapeLogic } = require("./scrapeLogic");
 
 app.get("/scrape", (req, res) => {
   scrapeLogic(res);
@@ -11,6 +9,8 @@ app.get("/scrape", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
 });
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
