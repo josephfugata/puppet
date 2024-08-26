15480bc6077964ed651b9079059d5416f1e59dfc
Joseph Fugata <34955657+josephfugata@users.noreply.github.com>
2024-08-26 05:25:46 +0000

    fix headless argument

--- genesysRefresh.js
+++ genesysRefresh.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const genesysRefresh = async (res) => {
     const getAuthHeader = async () => {
         const browser = await puppeteer.launch({
-            headless: false,
+            // headless: false,
             args: ["--disable-setuid-sandbox","--no-sandbox","--single-process","--no-zygote"],
             executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
         });
